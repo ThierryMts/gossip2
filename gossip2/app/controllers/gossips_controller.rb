38d2c1c6d4fcb69c1451a99704fccfe25bbdf2d2
SphinxToSETH <thierrymateus@hotmail.fr>
2017-11-07 22:54:54 +0100

    gossipv2

---
+++ gossip2/app/controllers/gossips_controller.rb
@@ -0,0 +1,53 @@
+class GossipsController < ApplicationController
+
+ def index
+    @gossips = Gossip.all
+  end
+ 
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+ 
+  def new
+    @gossip = Gossip.new
+  end
+ 
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+ 
+  def create
+    @gossip = Gossip.new(gossip_params)
+ 
+    if @gossip.save
+      redirect_to @gossip
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @gossip = Gossip.find(params[:id])
+ 
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+ 
+    redirect_to gossips_path
+  end
+
+ 
+  private
+    def gossip_params
+      params.require(:gossip).permit(:content)
+    end
+  
+end
+    
